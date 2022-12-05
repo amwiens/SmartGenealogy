b1c2520c1c1f4dcbe03b2cfbdb75cc68f82cdd43
Aaron Wiens <Wiens.Aaron@mayo.edu>
2022-12-05 14:21:41 -0600

    Starting mobile app.

---
+++ src/Mobile/SmartGenealogy.Mobile/SmartGenealogy.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartGenealogy.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/Mobile/SmartGenealogy.Mobile/SmartGenealogy.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartGenealogy.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
