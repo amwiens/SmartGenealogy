d5e5212b334be17a8a170db25f3b31f5e12462eb
Aaron Wiens <aaron@awwebsolutions.com>
2024-10-14 20:14:24 -0500

    Started Mvvm project.

---
+++ src/SmartGenealogy.Avalonia.Mvvm/ApplicationBase.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartGenealogy.Avalonia.Mvvm;
+
+public class ApplicationBase(
+    string organizationKey,
+    string applicationKey,
+    string uriString,
+    Type mainWindowType,
+    Type applicationModelType,
+    List<Type> modelTypes,
+    List<Type> singletonTypes,
+    List<Tuple<Type, Type>> servicesInterfaceAndType,
+    bool singleInstanceRequested = false) : Application, IApplicationBase
+{
+    public static Window MainWindow;
+
+    // The host cannot be null or else there is no app...
+    public static IHost AppHost { get; private set; }
+
+    // Logger will never be null or else
+}
