d89775cdf0b502230a2575d14a4b9fb7938635a7
Aaron Wiens <aaron@awwebsolutions.com>
2024-10-14 19:43:48 -0500

    Added model project.

---
+++ src/SmartGenealogy.Avalonia.Model/ApplicationModelBase.cs
@@ -0,0 +1,64 @@
+﻿namespace SmartGenealogy.Avalonia.Model;
+
+public class ApplicationModelBase(IProfiler profiler, ILogger logger, IApplicationBase application) : IApplicationModel
+{
+    protected readonly IProfiler profiler = profiler;
+    protected readonly ILogger logger = logger;
+    protected readonly IApplicationBase application = application;
+
+    public async Task Initialize()
+    {
+        try
+        {
+            foreach (var model in this.application.GetModels())
+            {
+                await model.Initialize();
+            }
+
+            this.logger.Info("Software initialization complete");
+        }
+        catch (Exception ex)
+        {
+            // Should never fail here
+            if (Debugger.IsAttached) { Debugger.Break(); }
+            this.logger.Error(ex.ToString());
+            throw new ApplicationException("Failed to initialize models.", ex);
+        }
+
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                // Delay until the app has fully started up before looking up the perf counters
+                // Do not collect, again to avoid a slow startup.
+                await Task.Delay(250);
+                this.profiler.MemorySnapshot("All Models Initialized", withGCCollect: false);
+            }
+            catch (Exception ex)
+            {
+                // Should never fail here
+                if (Debugger.IsAttached) { Debugger.Break(); }
+                this.logger.Error(ex.ToString());
+                throw new ApplicationException("Failed to cleanup on startup.", ex);
+            }
+        });
+    }
+
+    public async Task Shutdown()
+    {
+        try
+        {
+            foreach (var model in this.application.GetModels())
+            {
+                await model.Shutdown();
+            }
+        }
+        catch (Exception ex)
+        {
+            // Should never fail here
+            if (Debugger.IsAttached) { Debugger.Break(); }
+            this.logger.Error(ex.ToString());
+            throw;
+        }
+    }
+}
