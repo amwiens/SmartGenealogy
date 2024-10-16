82dc58afe6e6795a44abc32099f3f6be2df6b2ed
Aaron Wiens <aaron@awwebsolutions.com>
2024-10-15 22:08:24 -0500

    Added Persistence project.

---
+++ src/SmartGenealogy.Avalonia.Persistence/GlobalUsings.cs
@@ -0,0 +1,20 @@
+﻿global using System;
+global using System.Collections;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
+
+global using SmartGenealogy.Avalonia.Interfaces.Logger;
+global using SmartGenealogy.Avalonia.Interfaces.Messenger;
+global using SmartGenealogy.Avalonia.Interfaces.Model;
+global using SmartGenealogy.Avalonia.Interfaces.UserInterface;
+
+global using SmartGenealogy.Avalonia.Model;
