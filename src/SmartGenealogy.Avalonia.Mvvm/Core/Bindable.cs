12b14024764882b55b1b42dfad8caa325fe0190d
Aaron Wiens <aaron@awwebsolutions.com>
2024-10-16 07:17:03 -0500

    Working on MVVM project.

---
+++ src/SmartGenealogy.Avalonia.Mvvm/Core/Bindable.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartGenealogy.Avalonia.Mvvm.Core;
+
+[AttributeUsage(AttributeTargets.All)]
+public class DoNotLogAttribute : Attribute { }
+
+/// <summary>
+/// Bindable class, aka a View Model.
+/// </summary>
+/// <remarks>All bound properties are held in a dictionary.</remarks>
+public class Bindable : NotifyPropertyChanged
+{
+}
---
+++ src/SmartGenealogy.Avalonia.Mvvm/Core/Bindable{T}.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartGenealogy.Avalonia.Mvvm.Core;
+
+// TODO
+// Change Control to StyledElement which is the class that defines the DataContext property
+// Inheritance chain: Control -> InputElement -> Interactive -> Layoutable -> Visual -> StyledElement
+// Wait until V1 of Avalonia to avoid breaking changes
+
+/// <summary>
+/// Strongly typed bindable
+/// </summary>
+/// <typeparam name="TControl"></typeparam>
+public class Bindable<TControl> : Bindable where TControl : Control, new()
+{
+    public Bindable() : base() { }
+
+    public Bindable(bool disablePropertyChangedLogging = false, bool disableAutomaticBindingsLogging = false)
+        : base(disablePropertyChangedLogging, disableAutomaticBindingsLogging) { }
+
+    public Bindable(TControl control) : base() => this.Bind(control);
+
+    public void CreateViewAndBind()
+    {
+        var view = new TControl();
+        this.Bind(view);
+    }
+
+    public TControl View
+        => this.Control as TControl ?? throw new InvalidOperationException("View is null");
+}
