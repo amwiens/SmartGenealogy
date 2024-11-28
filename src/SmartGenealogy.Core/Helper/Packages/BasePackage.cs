7d5caccc5a040551aaa4aa3385afd7bb73973ef8
Aaron Wiens <aaron@awwebsolutions.com>
2024-11-27 22:54:40 -0600

    Added exception dialog and sentry logging.

---
+++ src/SmartGenealogy.Core/Helper/Packages/BasePackage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGenealogy.Core.Helper.Packages;
+
+public abstract class BasePackage
+{
+}
