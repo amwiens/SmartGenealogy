7755d2daa453c0059537b0f4a15934f313724aac
Aaron Wiens <aaron@awwebsolutions.com>
2024-12-03 19:08:45 -0600

    Updated package.

--- src/SmartGenealogy.UITests/TestAppBuilder.cs
+++ src/SmartGenealogy.UITests/TestAppBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 using NSubstitute;
 using NSubstitute.Extensions;
