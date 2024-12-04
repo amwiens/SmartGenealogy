7c4a2b598124d0f0e4d1fa24cc7f0747b2a5c0e2
Aaron Wiens <aaron@awwebsolutions.com>
2024-12-03 22:25:55 -0600

    Added TextBoxStyles.

---
+++ src/SmartGenealogy/Converters/FuncCommandConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+using PropertyModels.ComponentModel;
+
+namespace SmartGenealogy.Converters;
+
+/// <summary>
+/// Converts an object's named <see cref="Func{TResult}"/> to a <see cref="ICommand"/>.
+/// </summary>
+public class FuncCommandConverter : IValueConverter
+{
+    /// <inheritdoc />
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null)
+        {
+            return null;
+        }
+
+        // Parameter is the name of the Func<T> to convert.
+        if (parameter is not string funcName)
+        {
+            throw new ArgumentException("Parameter must be a string.", nameof(parameter));
+        }
+
+        // Find the Func<T> on the object.
+        if (value.GetType().GetMethod(funcName) is not { } methodInfo)
+        {
+            throw new ArgumentException(
+                $"Method {funcName} not found on {value.GetType().Name}.",
+                nameof(parameter));
+        }
+
+        // Create a delegate from the method info.
+        var func = (Action)methodInfo.CreateDelegate(typeof(Action), value);
+
+        // Create ICommand
+        var command = ReactiveCommand.Create(func);
+
+        return command;
+    }
+
+    /// <inheritdoc />
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
