d41dff85a5bbd833cecd3627041879b6043a0b47
Aaron Wiens <aaron@awwebsolutions.com>
2024-12-03 19:56:28 -0600

    Added Icon.

--- src/SmartGenealogy/Assets.cs
+++ src/SmartGenealogy/Assets.cs
@@ -4,6 +4,11 @@ namespace SmartGenealogy;
 
 internal static class Assets
 {
+    public static AvaloniaResource AppIcon { get; } =
+        new("avares://SmartGenealogy/Assets/Icon.ico");
+
+    public static AvaloniaResource AppIconPng { get; } =
+        new("avares://SmartGenealogy/Assets/Icon.png");
 
     public static AvaloniaResource ImagePromptLanguageJson =>
         new("avares://SmartGenealogy/Assets/ImagePrompt.tmLanguage.json");
