bc94dfec6a35e0e2a7dbfdf3833f43c57ec25b0b
Aaron Wiens <aaron@awwebsolutions.com>
2025-03-18 06:31:36 -0500

    Started updating the styles for the application.

--- src/SmartGenealogy/MauiProgram.cs
+++ src/SmartGenealogy/MauiProgram.cs
@@ -28,9 +28,21 @@ public static class MauiProgram
 			.UseSkiaSharp()
             .ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Poppins-Regular.otf", "RegularFont");
+                fonts.AddFont("Poppins-Medium.otf", "MediumFont");
+                fonts.AddFont("Poppins-SemiBold.otf", "SemiBoldFont");
+                fonts.AddFont("Poppins-Bold.otf", "BoldFont");
+                fonts.AddFont("Caveat-Bold.ttf", "HandwritingBoldFont");
+                fonts.AddFont("Caveat-Regular.ttf", "HandwritingFont");
+
+                fonts.AddFont("fa-solid-900.ttf", "FaPro");
+                fonts.AddFont("fa-brands-400.ttf", "FaBrands");
+                fonts.AddFont("fa-regular-400.ttf", "FaRegular");
+                fonts.AddFont("line-awesome.ttf", "LineAwesome");
+                fonts.AddFont("material-icons-outlined-regular.otf", "MaterialDesign");
+                fonts.AddFont("ionicons.ttf", "IonIcons");
+                fonts.AddFont("icon.ttf", "SmartGenealogyIcons");
+            });
 
 		// Services
 		builder.Services.AddSingleton<GeocodeService>();
