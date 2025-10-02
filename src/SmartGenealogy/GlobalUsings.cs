e9b445ecd2c4e5f95a9875a48581c3cbf5d40ecc
Aaron Wiens <aaron@awwebsolutions.com>
2025-10-02 18:12:44 -0500

    #1 Added project and cleaned up code.

---
+++ src/SmartGenealogy/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using CommunityToolkit.Maui;
+global using CommunityToolkit.Mvvm.ComponentModel;
+
+global using Microsoft.Extensions.Logging;
+
+global using SmartGenealogy.ViewModels;
+global using SmartGenealogy.Views;
