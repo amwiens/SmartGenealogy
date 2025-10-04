4763d9f7abc95b86a7154e183974145b1ca7b550
Aaron Wiens <aaron@awwebsolutions.com>
2025-10-04 11:54:06 -0500

    #10 Added ability to open an existing database.

--- src/SmartGenealogy.Core/ViewModels/Main/MainPageViewModel.cs
+++ src/SmartGenealogy.Core/ViewModels/Main/MainPageViewModel.cs
@@ -48,7 +48,43 @@ public partial class MainPageViewModel(
     [RelayCommand]
     private async Task OpenDatabase()
     {
+        try
+        {
+            var databaseFileType = new FilePickerFileType(
+                new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.WinUI, new[] { ".sgdb" } }
+                });
 
+            var options = new PickOptions
+            {
+                PickerTitle = "Please select a file",
+                FileTypes = databaseFileType
+            };
+
+            var result = await FilePicker.Default.PickAsync(options);
+
+            if (result != null)
+            {
+                if (File.Exists(result.FullPath) && IsSQLiteDatabase(result.FullPath))
+                {
+                    var fileInfo = new FileInfo(result.FullPath);
+
+                    databaseSettings.DatabaseFilename = fileInfo.Name;
+                    databaseSettings.DatabasePath = fileInfo.Directory!.FullName;
+                    DatabaseOpen = true;
+                }
+                else
+                {
+                    if (Shell.Current is Shell shell)
+                        await shell.DisplayAlertAsync("Error", "Unable to open database file.", "OK");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            modalErrorHandler.HandleError(ex);
+        }
     }
 
     /// <summary>
@@ -60,5 +96,33 @@ public partial class MainPageViewModel(
         databaseSettings.DatabaseFilename = null;
         databaseSettings.DatabasePath = null;
         DatabaseOpen = false;
+    }
+
+    private bool IsSQLiteDatabase(string fileName)
+    {
+        var sQLiteHeader = System.Text.Encoding.ASCII.GetBytes("SQLite format 3\0");
+        if (!File.Exists(fileName))
+            throw new FileNotFoundException("The specified file does not exist.");
+
+        var fileHeader = new byte[16];
+        using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+        {
+            if (fs.Length < 16)
+                return false;
+
+            fs.ReadExactly(fileHeader, 0, 16);
+        }
+
+        // Compare Headers
+        if (sQLiteHeader.Length != fileHeader.Length)
+            return false;
+
+        for (int i = 0; i <  sQLiteHeader.Length; i++)
+        {
+            if (sQLiteHeader[i] != fileHeader[i])
+                return false;
+        }
+
+        return true;
     }
 }
