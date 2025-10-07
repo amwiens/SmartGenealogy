a3463c905572d082d95fa84e8768562c1d617d6f
Aaron Wiens <aaron@awwebsolutions.com>
2025-10-06 21:43:06 -0500

    #19 Added database reindex and integrity check.

--- src/SmartGenealogy.Core/ViewModels/Tools/ToolsPageViewModel.cs
+++ src/SmartGenealogy.Core/ViewModels/Tools/ToolsPageViewModel.cs
@@ -24,6 +24,38 @@ public partial class ToolsPageViewModel(DatabaseTools databaseTools, ModalErrorH
     }
 
     /// <summary>
+    /// Reindex database.
+    /// </summary>
+    [RelayCommand]
+    private async Task ReindexDatabase()
+    {
+        try
+        {
+            await databaseTools.ReindexDatabase();
+        }
+        catch (Exception ex)
+        {
+            errorHandler.HandleError(ex);
+        }
+    }
+
+    /// <summary>
+    /// Database integrity check.
+    /// </summary>
+    [RelayCommand]
+    private async Task DatabaseIntegrityCheck()
+    {
+        try
+        {
+            await databaseTools.DatabaseIntegrityCheck();
+        }
+        catch (Exception ex)
+        {
+            errorHandler.HandleError(ex);
+        }
+    }
+
+    /// <summary>
     /// Open Fact Types page.
     /// </summary>
     [RelayCommand]
--- src/SmartGenealogy.Data/Repositories/DatabaseTools.cs
+++ src/SmartGenealogy.Data/Repositories/DatabaseTools.cs
@@ -7,7 +7,9 @@
 /// <param name="logger">Logger</param>
 public class DatabaseTools(DatabaseSettings databaseSettings, ILogger<DatabaseTools> logger)
 {
-
+    /// <summary>
+    /// Compact database.
+    /// </summary>
     public async Task CompactDatabase()
     {
         await using var connection = new SqliteConnection(databaseSettings.ConnectionString);
@@ -23,6 +25,50 @@ public class DatabaseTools(DatabaseSettings databaseSettings, ILogger<DatabaseTo
         catch (Exception ex)
         {
             logger.LogError(ex, "Error compacting database.");
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Reindex database.
+    /// </summary>
+    public async Task ReindexDatabase()
+    {
+        await using var connection = new SqliteConnection(databaseSettings.ConnectionString);
+        await connection.OpenAsync();
+
+        try
+        {
+            var compactCommand = connection.CreateCommand();
+            compactCommand.CommandText = "REINDEX;";
+
+            await compactCommand.ExecuteNonQueryAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error rebuiding indexes in database.");
+            throw;
+        }
+    }
+
+    /// <summary>
+    /// Database integrity check.
+    /// </summary>
+    public async Task DatabaseIntegrityCheck()
+    {
+        await using var connection = new SqliteConnection(databaseSettings.ConnectionString);
+        await connection.OpenAsync();
+
+        try
+        {
+            var compactCommand = connection.CreateCommand();
+            compactCommand.CommandText = "PRAGMA integrity_check;";
+
+            await compactCommand.ExecuteNonQueryAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error running integrity check in database.");
             throw;
         }
     }
