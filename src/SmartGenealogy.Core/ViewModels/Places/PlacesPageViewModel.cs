447d9c1dfa7f4dce556da21c9a60e379157af308
Aaron Wiens <aaron@awwebsolutions.com>
2025-10-07 21:19:01 -0500

    #30 Added code to query places.

--- src/SmartGenealogy.Core/ViewModels/Places/PlacesPageViewModel.cs
+++ src/SmartGenealogy.Core/ViewModels/Places/PlacesPageViewModel.cs
@@ -1,5 +1,47 @@
 ﻿namespace SmartGenealogy.Core.ViewModels.Places;
 
-public partial class PlacesPageViewModel : ObservableObject
+/// <summary>
+/// Places page view model.
+/// </summary>
+/// <param name="placeRepository">Place repository</param>
+/// <param name="popupService">Popup service</param>
+/// <param name="errorHandler">Modal error handler</param>
+public partial class PlacesPageViewModel(
+    PlaceRepository placeRepository,
+    IPopupService popupService,
+    ModalErrorHandler errorHandler) : ObservableObject
 {
+    [ObservableProperty]
+    private ObservableCollection<Place> _places = [];
+
+    [ObservableProperty]
+    private Place? _selectedItem;
+
+    /// <summary>
+    /// Appearing
+    /// </summary>
+    [RelayCommand]
+    private async Task Appearing()
+    {
+        Places = new ObservableCollection<Place>(await placeRepository.ListAsync());
+    }
+
+    /// <summary>
+    /// Add place.
+    /// </summary>
+    /// <returns></returns>
+    [RelayCommand]
+    private async Task AddPlace()
+    {
+
+    }
+
+    /// <summary>
+    /// Open place details
+    /// </summary>
+    [RelayCommand]
+    private async Task OpenPlaceDetails()
+    {
+
+    }
 }
