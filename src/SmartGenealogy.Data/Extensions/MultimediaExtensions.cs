379fe738ba64c34a0458e2d5e67caee30f928c63
Aaron Wiens <aaron@awwebsolutions.com>
2025-10-08 19:47:41 -0500

    #35 Added multimedia database objects.

---
+++ src/SmartGenealogy.Data/Extensions/MultimediaExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartGenealogy.Data.Extensions;
+
+/// <summary>
+/// Multimedia extensions
+/// </summary>
+public static class MultimediaExtensions
+{
+    /// <summary>
+    /// Check if the multimedia is null or new.
+    /// </summary>
+    /// <param name="multimedia">Multimedia</param>
+    /// <returns><see langword="true"> if null or new, otherwise <see langword="false"/>.</returns>
+    public static bool IsNullOrNew([NotNullWhen(false)] this Multimedia? multimedia)
+    {
+        return multimedia is null || multimedia.Id == 0;
+    }
+}
---
+++ src/SmartGenealogy.Data/Models/Multimedia.cs
@@ -0,0 +1,73 @@
+﻿namespace SmartGenealogy.Data.Models;
+
+/// <summary>
+/// Multimedia
+/// </summary>
+public class Multimedia
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Media type
+    /// </summary>
+    public MediaType MediaType { get; set; }
+
+    /// <summary>
+    /// Media path
+    /// </summary>
+    public string? MediaPath { get; set; }
+
+    /// <summary>
+    /// Media file
+    /// </summary>
+    public string? MediaFile { get; set; }
+
+    /// <summary>
+    /// Url
+    /// </summary>
+    public string? Url { get; set; }
+
+    /// <summary>
+    /// Thumbnail
+    /// </summary>
+    public byte[]? Thumbnail { get; set; }
+
+    /// <summary>
+    /// Caption
+    /// </summary>
+    public string? Caption { get; set; }
+
+    /// <summary>
+    /// Ref number
+    /// </summary>
+    public string? RefNumber { get; set; }
+
+    /// <summary>
+    /// Date
+    /// </summary>
+    public string? Date { get; set; }
+
+    /// <summary>
+    /// Sort date
+    /// </summary>
+    public int SortDate { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Date added to the database
+    /// </summary>
+    public DateTime DateAdded { get; set; }
+
+    /// <summary>
+    /// Date changed
+    /// </summary>
+    public DateTime DateChanged { get; set; }
+
+}
