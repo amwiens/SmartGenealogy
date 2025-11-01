fe42072f5cbf6744e8c2a8eb789c23916f877aa3
Aaron Wiens <aaron@awwebsolutions.com>
2025-11-01 08:59:48 -0500

    #114 Added comments to enums.

--- src/SmartGenealogy.Data/Enums/ProjectCategory.cs
+++ src/SmartGenealogy.Data/Enums/ProjectCategory.cs
@@ -1,13 +1,25 @@
 ﻿namespace SmartGenealogy.Data.Enums;
 
+/// <summary>
+/// Project category
+/// </summary>
 public enum ProjectCategory
 {
+    /// <summary>
+    /// Research
+    /// </summary>
     [Description("Research")]
     Research = 1,
 
+    /// <summary>
+    /// To-do
+    /// </summary>
     [Description("To-Do")]
     ToDo = 2,
 
+    /// <summary>
+    /// Correspondence
+    /// </summary>
     [Description("Correspondence")]
     Correspondence = 3,
 }
--- src/SmartGenealogy.Data/Enums/ProjectStatus.cs
+++ src/SmartGenealogy.Data/Enums/ProjectStatus.cs
@@ -5,8 +5,23 @@
 /// </summary>
 public enum ProjectStatus
 {
+    /// <summary>
+    /// New
+    /// </summary>
     New = 0,
+
+    /// <summary>
+    /// In progress
+    /// </summary>
     InProgress = 1,
+
+    /// <summary>
+    /// In review
+    /// </summary>
     InReview = 2,
+
+    /// <summary>
+    /// Completed
+    /// </summary>
     Completed = 3
 }
